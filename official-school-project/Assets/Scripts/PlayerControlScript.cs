d971c52abfd5e2a73f9f198112a5bdc4fa9b35de
ooflemon <cubewang0701@gmail.com>
2024-12-15 07:57:02 +0800

    create PlayerControlScript
    add variable region, physic region

--- official-school-project/Assets/Scripts/PlayerControlScript.cs
+++ official-school-project/Assets/Scripts/PlayerControlScript.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class PlayerControlScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	//variables
+	#region variable
+	//reference
+	private Rigidbody2D rb;
+
+	//physics
+	private float myGravityScale;
+	private float myGravityMaxSpeed; // max fall speed
+
+
+	#endregion
+
+
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+        //init
+		rb = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +32,14 @@ public class PlayerControlScript : MonoBehaviour
     {
         
     }
+
+	#region physic function
+
+	private void physicMain()
+	{
+
+	}
+
+	#endregion
+
 }
