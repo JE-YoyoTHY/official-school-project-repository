d2f985d183909ff6cab304600c7b183e622b8932
GsawHuang <gsaw0820@gmail.com>
2025-03-09 15:51:00 +0800

    增加一個會跟隨鏡頭做微小移動的雲
    還沒做完

---
+++ official-school-project/Assets/Scripts/Background/MovingClouds.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CloudParallax : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    public float parallaxEffect = 0.5f;
+
+    void Start(){
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    void Update(){
+        float deltaX = cameraTransform.position.x - lastCameraPosition.x;
+        transform.position += new Vector3(deltaX * parallaxEffect, 0, 0);
+        lastCameraPosition = cameraTransform.position;
+    }
+}
