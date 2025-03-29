08cc51cf872bfbf46ffe6f804eedca698ad1e565
ooflemon <cubewang0701@gmail.com>
2025-03-29 15:07:36 +0800

    modify cloud (失敗)

--- official-school-project/Assets/Scripts/Background/CloudSpawner.cs
+++ official-school-project/Assets/Scripts/Background/CloudSpawner.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 //by GPT, 還沒修過
+
+//by ooflemon
+/* 每隔一段時間會生成一次雲(outer cycle
+ * 每一次生成雲會生成不特定的數量(inner cycle
+ * 生成多個雲的時候，每一朵雲會有自己的間隔
+ * back to outer cycle
+ */
+
+//現在有bug
+
 public class CloudSpawner : MonoBehaviour
 {
-    public GameObject cloudPrefab;  // 連結你的雲朵Prefab
+	//ooflemon rewrite the code
+	/*public GameObject cloudPrefab;  // 連結你的雲朵Prefab
     public float spawnInterval = 2f; // 產生間隔
     public float cloudSpeed = 1f; // 雲朵移動速度
     public float minY = -2f, maxY = 2f; // 雲朵 Y 軸範圍
@@ -38,5 +50,116 @@ public class CloudSpawner : MonoBehaviour
 
         cloud.GetComponent<Rigidbody2D>().velocity = new Vector2(-cloudSpeed, 0);
         Destroy(cloud, 10f); // 10秒後刪除，防止物件堆積
-    }
+    }*/
+
+
+	//variables
+	private RectTransform rect;
+
+	[SerializeField] private bool isSpawner = false; // if true, spawner, else -> moving prefab
+
+
+	[SerializeField] private GameObject cloudPrefab;
+
+	[SerializeField] private float spawnRateMin, spawnRateMax; //outer cycle
+	[SerializeField] private int spawnAmountMin, spawnAmountMax;
+	[SerializeField] private float innerCycleIntervalMin, innerCycleIntervalMax; // inner cycle interval
+	[SerializeField] private float YRange;
+	private float spawnRateCounter = 0, spawnAmountCounter = 0, innerCycleTimeCounter = 0;
+
+	private GameObject prefabInstance;
+	[SerializeField] private float prefabMoveSpeedMin, prefabMoveSpeedMax;
+	private float moveSpeed;
+	//[SerializeField] private Object[] images;
+
+
+	private void Start()
+	{
+		rect = GetComponent<RectTransform>();
+	}
+
+	private void Update()
+	{
+		if (!LogicScript.instance.isFreeze())
+		{
+			if (isSpawner)
+				spawnMain();
+			else moveMain();
+		}
+	}
+
+	private void spawnMain()
+	{
+		if(spawnRateCounter <= 0) // outer cycle
+		{
+			if(spawnAmountCounter <= 0) //inner cycle
+			{
+				innerCycleStart();
+			}
+			else
+			{
+				if(innerCycleTimeCounter <= 0)
+				{
+					spawnPrefab();
+				}
+				else
+				{
+					innerCycleTimeCounter -= Time.deltaTime;
+				}
+			}
+		}
+		else
+		{
+			spawnRateCounter -= Time.deltaTime;
+		}
+	}
+
+	private void innerCycleStart()
+	{
+		spawnAmountCounter = Random.Range(spawnAmountMin, spawnAmountMax);
+	}
+
+	private void spawnPrefab()
+	{
+		//Vector3 pos = rect.position;
+		//pos.y = pos.y + Random.Range(-YRange, YRange);
+
+		//spawn
+		prefabInstance = Instantiate(cloudPrefab, transform.parent);
+		prefabInstance.GetComponent<CloudSpawner>().prefabInit(Random.Range(prefabMoveSpeedMin, prefabMoveSpeedMax), Random.Range(-YRange, YRange));
+
+		//inner cycle
+		spawnAmountCounter--;
+		if(spawnAmountCounter > 0)
+		{
+			innerCycleTimeCounter = Random.Range(innerCycleIntervalMin, innerCycleIntervalMax);
+		}
+		else
+		{
+			spawnRateCounter = Random.Range(spawnRateMin, spawnRateMax);
+		}
+	}
+
+
+	#region prefab
+
+	private void moveMain()
+	{
+		rect.position = rect.position - new Vector3(moveSpeed, 0, 0);
+
+		if (rect.position.x < -3000)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void prefabInit(float speed, float y)
+	{
+		moveSpeed = speed * Time.deltaTime;
+		rect = GetComponent<RectTransform>();
+		rect.position = new Vector3(1000f, y, 0f);
+	}
+
+	#endregion
+
 }
