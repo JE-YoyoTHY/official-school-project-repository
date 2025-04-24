e55709eaad500272e1240214381c4f1cd5d73415
GsawHuang <gsaw0820@gmail.com>
2025-04-24 13:02:23 +0800

    complete background change

--- official-school-project/Assets/Scripts/Background/BackgroundChange.cs
+++ official-school-project/Assets/Scripts/Background/BackgroundChange.cs
@@ -1,11 +1,18 @@
-/*using UnityEngine;
+using UnityEngine;
 
-public class BackgroundFollow : MonoBehaviour
+public class BackgroundChange : MonoBehaviour
 {
-    [SerializeField] private Sprite[] backgroundImages;
-    
-    public swapBackground(int i){ //from current to i
+    //[SerializeField] private Sprite[] backgroundImages;
+    [SerializeField] private GameObject[] backgroundImages;
+    private int currentIndex = 0;
 
+    public void swapBackground(int i){ //from current to i
+        //disable current
+        backgroundImages[i].SetActive(false);
+
+        //enable next
+        currentIndex = i;
+        backgroundImages[i].SetActive(true);
     }
 
-}*/
+}
