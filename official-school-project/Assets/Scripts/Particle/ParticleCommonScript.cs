e57c2ebf02ca0c1f7fb2a8a457285b828f43d984
ooflemon <cubewang0701@gmail.com>
2025-05-29 22:32:15 +0800

    adjust chapter 3 and made jump and fall particle

--- official-school-project/Assets/Scripts/Particle/ParticleCommonScript.cs
+++ official-school-project/Assets/Scripts/Particle/ParticleCommonScript.cs
@@ -8,8 +8,15 @@ public class ParticleCommonScript : MonoBehaviour
 	[SerializeField] private ParticleSystem ParticleSystem;
 	private ParticleSystem ParticleInstance;
 
+    [SerializeField] private ParticleSystem[] ParticleSystemWithIndex;
+
     public void emitParticle(){
         //particle
 		ParticleInstance = Instantiate(ParticleSystem, transform.position, Quaternion.identity);
+    }
+
+    public void emitParticleWithIndex(int i)
+    {
+        ParticleInstance = Instantiate(ParticleSystemWithIndex[i], transform.position, Quaternion.identity);
     }
 }
