105c75db95596570367a240004e01266af1f1c67
ooflemon <cubewang0701@gmail.com>
2025-06-05 14:48:20 +0800

    modify dark clouds

--- official-school-project/Assets/Scripts/Levels/Boss/ZeusPowerCrystalInstanceScript.cs
+++ official-school-project/Assets/Scripts/Levels/Boss/ZeusPowerCrystalInstanceScript.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class ZeusPowerCrystalInstanceScript : MonoBehaviour
 {
     private ZeusPowerCrystalManagerScript crystalManager;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Sprite[] sprites;
+
+    [Header("floating")]
+    private float floatDistanceMin = 0.7f; // y
+    private float floatDistanceMax = 0.4f; // y
+    private float floatTimeMin = 3.5f;
+    private float floatTimeMax = 6.0f;
+    private float currentFloatAngle; //SHM, using sine to float
+    private float floatInitialPosY;
+
+    private float currentFloatTime;
+    private float currentFloatDistance;
 
     //private bool isDestroyed = false;
 
@@ -12,12 +26,30 @@ public class ZeusPowerCrystalInstanceScript : MonoBehaviour
     void Start()
     {
         crystalManager = transform.parent.GetComponent<ZeusPowerCrystalManagerScript>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.sprite = sprites[Random.Range((int)0, sprites.Length)];
+
+        floatInitialPosY = transform.position.y;
+
+        currentFloatTime = Random.Range(floatTimeMin, floatTimeMax);
+        currentFloatDistance = Random.Range(floatDistanceMin, floatDistanceMax);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        transform.position = new Vector3(transform.position.x, floatInitialPosY + Mathf.Sin(currentFloatAngle * Mathf.Deg2Rad) * currentFloatDistance, transform.position.z);
         
+        currentFloatAngle += Time.fixedDeltaTime * (360 / currentFloatTime);
+        while (currentFloatAngle > 360)
+        {
+            currentFloatAngle -= 360;
+            
+            //random
+            currentFloatTime = Random.Range(floatTimeMin, floatTimeMax);
+            currentFloatDistance = Random.Range(floatDistanceMin, floatDistanceMax);
+        } 
     }
 
     public void crystalInstanceDestroy()
